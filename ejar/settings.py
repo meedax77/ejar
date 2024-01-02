4a3c99ea270448806fe416e13a5184dd5d855c58
meedax77 <loodxloodx@gmail.com>
2024-01-02 17:40:21 +0300

    Your commit message here

--- ejar/settings.py
+++ ejar/settings.py
@@ -154,7 +154,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ejar/static')
 ]
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
